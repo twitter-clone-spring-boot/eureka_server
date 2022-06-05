daa58142daabef7afc7b1c69aa918a3121850d70
Sanchit Bansal <sanchitbansal10@gmail.com>
2022-06-05 15:42:39 +0530

    Dummy commit to trigger CI

--- src/main/java/twitter/clone/eureka_server/EurekaServerApplication.java
+++ src/main/java/twitter/clone/eureka_server/EurekaServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//comment out
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
